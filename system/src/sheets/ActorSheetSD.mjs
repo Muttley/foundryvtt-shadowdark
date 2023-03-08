81509d16e912a72f4f3566b56084d1be2b525f91
Paul Maskelyne <mu77ley@gmail.com>
2023-03-08 10:16:40 +0000

    inventory context menu for add/delete items

--- system/src/sheets/ActorSheetSD.mjs
+++ system/src/sheets/ActorSheetSD.mjs
@@ -1,5 +1,14 @@
 export default class ActorSheetSD extends ActorSheet {
 
+	/** @inheritdoc */
+	activateListeners(html) {
+		// Handle default listeners last so system listeners are triggered first
+		super.activateListeners(html);
+
+		// Create context menu for items on both sheets
+		this._contextMenu(html);
+	}
+
 	/** @override */
 	async getData(options) {
 		const source = this.actor.toObject();
@@ -32,4 +41,80 @@ export default class ActorSheetSD extends ActorSheet {
 
 		return context;
 	}
+
+	_contextMenu(html) {
+		ContextMenu.create(this, html, ".item", this._getItemContextOptions());
+	}
+
+	_getItemContextOptions() {
+		const canEdit = function(tr) {
+			let result = false;
+			const itemId = tr.data("item-id");
+
+			if (game.user.isGM) {
+				result = true;
+			}
+			else {
+				result = this.actor.items.find(item => item._id === itemId)
+					? true
+					: false;
+			}
+
+			return result;
+		};
+
+		return [
+			{
+				name: game.i18n.localize("SHADOWDARK.sheet.general.item_edit.title"),
+				icon: '<i class="fas fa-edit"></i>',
+				condition: tr => canEdit(tr),
+				callback: tr => {
+					const itemId = tr.data("item-id");
+					const item = this.actor.items.get(itemId);
+					return item.sheet.render(true);
+				},
+			},
+			{
+				name: game.i18n.localize("SHADOWDARK.sheet.general.item_delete.title"),
+				icon: '<i class="fas fa-trash"></i>',
+				condition: tr => canEdit(tr),
+				callback: tr => {
+					const itemId = tr.data("item-id");
+					this._onItemDelete(itemId);
+				},
+			},
+		];
+	}
+
+	_onItemDelete(itemId) {
+		const itemData = this.actor.getEmbeddedDocument("Item", itemId);
+
+		renderTemplate(
+			"systems/shadowdark/templates/dialog/delete-item.hbs",
+			{name: itemData.name}
+		).then(html => {
+			new Dialog({
+				title: "Confirm Deletion",
+				content: html,
+				buttons: {
+					Yes: {
+						icon: "<i class=\"fa fa-check\"></i>",
+						label: "Yes",
+						callback: async () => {
+							await this.actor.deleteEmbeddedDocuments(
+								"Item",
+								[itemId]
+							);
+						},
+					},
+					Cancel: {
+						icon: "<i class=\"fa fa-times\"></i>",
+						label: "Cancel",
+					},
+				},
+				default: "Yes",
+			}).render(true);
+		});
+	}
+
 }
