562339d39d8b0e9b535f156508551698d013b511
bakbakbakbakbak <bakbakbakbak@protonmail.com>
2023-03-08 18:05:15 +0100

    chore(test): Added item document tests

--- system/src/testing/index.mjs
+++ system/src/testing/index.mjs
@@ -6,12 +6,21 @@ import documentsActorTests, {
 	key as documentsActorKey,
 	options as documentsActorOptions,
 } from "../documents/__tests__/documents-actor.test.mjs";
-
+import documentsItemsTests, {
+	key as documentsItemsKey,
+	options as documentsItemsOptions,
+} from "../documents/__tests__/documents-item.test.mjs";
 
 Hooks.on("quenchReady", async quench => {
+	// Document tests
 	quench.registerBatch(
 		documentsActorKey,
 		documentsActorTests,
 		documentsActorOptions
+	);
+	quench.registerBatch(
+		documentsItemsKey,
+		documentsItemsTests,
+		documentsItemsOptions
 	);
 });
