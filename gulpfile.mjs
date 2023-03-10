02ddd5fcd98baf4b3e2a75a52c2edd0fc6266546
bakbakbakbakbak <bakbakbakbak@protonmail.com>
2023-03-10 17:19:08 +0100

    feat(items): Spell compendium

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -3,19 +3,22 @@ import gulp from "gulp";
 import * as css from "./utils/css.mjs";
 import * as lang from "./utils/lang.mjs";
 import * as javascript from "./utils/javascript.mjs";
+import * as packs from "./utils/packs.mjs";
 
 export default gulp.series(
 	gulp.parallel(
 		css.compile,
 		lang.compile,
 		javascript.lint,
-		javascript.compile
+		javascript.compile,
+		packs.compile
 	),
 
 	gulp.parallel(
 		css.watchUpdates,
 		lang.watchUpdates,
-		javascript.watchUpdates
+		javascript.watchUpdates,
+		packs.watchUpdates
 	)
 );
 
@@ -23,10 +26,11 @@ export const build = gulp.parallel(
 	css.compile,
 	lang.compile,
 	javascript.lint,
-	javascript.compile
+	javascript.compile,
+	packs.compile
 );
 
-export const clean = gulp.parallel(css.clean, lang.clean);
+export const clean = gulp.parallel(css.clean, lang.clean, packs.clean);
 export const compileCss = gulp.series(css.compile);
 export const compileLang = gulp.series(lang.compile);
 export const lintJs = gulp.series(javascript.lint);
