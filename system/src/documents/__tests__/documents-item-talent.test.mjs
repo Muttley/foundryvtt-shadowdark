4168929d5296602a653ef3de27d31b06bc6bc24e
bakbakbakbakbak <bakbakbakbak@protonmail.com>
2023-03-17 10:38:43 +0100

    chore(test): Added skeleton for Talent tests

---
+++ system/src/documents/__tests__/documents-item-talent.test.mjs
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-expressions */
+/**
+ * @file Contains tests for talent item documents
+ */
+import ItemSD from "../ItemSD.mjs";
+import { cleanUpItemsByKey, closeDialogs, openDialogs, trashChat, waitForInput } from "../../testing/testUtils.mjs";
+
+export const key = "shadowdark.documents.item.talent";
+export const options = {
+	displayName: "ShadowDark: Documents: Item, Talent",
+};
+
+export default ({ describe, it, after, beforeEach, before, expect }) => {};
