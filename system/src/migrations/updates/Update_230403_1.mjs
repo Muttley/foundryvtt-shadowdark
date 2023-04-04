d202512a115dced4ee5dd2c996d2a0e6e32cc8ad
Paul Maskelyne <mu77ley@gmail.com>
2023-04-04 23:09:48 +0100

    migrate talent effects both in the world and on actors

--- system/src/migrations/updates/Update_230403_1.mjs
+++ system/src/migrations/updates/Update_230403_1.mjs
@@ -16,8 +16,6 @@ export default class Update_230403_1 extends UpdateBaseSD {
 		const abilityKeys = CONFIG.SHADOWDARK.ABILITY_KEYS;
 		abilityKeys.forEach(ability => bonuses[ability] = 0);
 
-		console.log(bonuses);
-
 		const items = actor.items;
 		for (const item of items) {
 			if (item.type === "Talent") {
@@ -36,8 +34,6 @@ export default class Update_230403_1 extends UpdateBaseSD {
 			updateData[`system.abilities.${ability}.-=value`] = null;
 			updateData[`system.abilities.${ability}.base`] = value - bonuses[ability];
 		}
-		// Delete `flags` from system.
-		// updateData["system.-=flags"] = null;
 
 		return updateData;
 	}
@@ -45,23 +41,66 @@ export default class Update_230403_1 extends UpdateBaseSD {
 	async updateItem(itemData, actorData) {
 		if (itemData.type !== "Talent") return;
 
-		const updateData = {};
-
+		let actor;
 		let item;
 		if (actorData) {
-			const actor = game.actors.get(actorData._id);
+			actor = game.actors.get(actorData._id);
 			item = actor.items.get(itemData._id);
 		}
 		else {
 			item = game.items.get(itemData._id);
 		}
 
-		if (item?.hasProperty("abilityImprovement")) {
+		// Nothing to do if its not an ability improvement
+		if (!item?.hasProperty("abilityImprovement")) return;
+
+		const updateData = {};
+
+		if (actorData) {
+			// We can't update the settings of active effects attached to an
+			// Actor, so we have to delete the existing Talent item and replace
+			// it with an already updated one from the system compendiums
+			//
+			const itemName = item.name;
+
+			shadowdark.log(`Replacing out of date Talent '${itemName}' belonging to Actor '${actorData.name}`);
+
+			await actor.deleteEmbeddedDocuments("Item", [item._id]);
+
+			const talentsPack = game.packs.get("shadowdark.talents");
+			const talentId = talentsPack.index.find(i => i.name === itemName)._id;
+			const talent = await talentsPack.getDocument(talentId);
+			await actor.createEmbeddedDocuments("Item", [talent]);
+		}
+		else {
+			// We can just upgrade the active effect settings for items not
+			// attached to an actor
+			//
 			for (const ability of CONFIG.SHADOWDARK.ABILITY_KEYS) {
 				const value = Number(itemData.system.abilities[ability].value || 0);
 				updateData[`system.abilities.${ability}.-=value`] = null;
 				updateData[`system.abilities.${ability}.bonus`] = value;
 			}
+
+			const effect = item.effects.find(e => e.label === "abilityImprovement");
+
+			const re = /(system.abilities.[a-z]{3}).value/;
+
+			const changes = [];
+			for (const change of effect.changes) {
+				const key = change.key;
+				const match = key.match(re);
+
+				if (match) {
+					change.key = `${match[1]}.bonus`;
+					change.value = change.value === "" ? "0" : change.value;
+				}
+
+				changes.push(change);
+			}
+
+			await effect.update({changes});
+
 		}
 
 		return updateData;
