ab26cc7ba84c0630e93481b8e58121e8171d1f71
bakbakbakbakbak <bakbakbakbak@protonmail.com>
2023-04-13 06:46:42 +0200

    finished

--- utils/dbToJson.py
+++ utils/dbToJson.py
@@ -4,7 +4,8 @@ import os
 import json
 import re
 
-dbFolder="../system/packs/_convert"
+dbFolder="../system/packs"
+resultFolder="../system/assets/db/new"
 
 def slugify(s):
   s = s.lower().strip()
@@ -13,9 +14,10 @@ def slugify(s):
   s = re.sub(r'^-+|-+$', '', s)
   return s
 
-for filepath in glob.iglob(dbFolder + "/*.db"):
+for filepath in glob.iglob(dbFolder + "/rollable-tables.db"):
   filename, file_extension = os.path.splitext(os.path.basename(filepath))
-  resultFolder = dbFolder + "/" + filename
+  resultFile = resultFolder + "/" + filename
+
   if not os.path.exists(resultFolder):
     os.makedirs(resultFolder)
 
@@ -23,7 +25,24 @@ for filepath in glob.iglob(dbFolder + "/*.db"):
     lines = f.readlines()
     for line in lines:
       data = json.loads(line)
+      # Setup for rollable tables
+      newData = {
+        "_id": data["_id"],
+        "name": data["name"],
+        "img": data["img"],
+        "description": data["description"],
+        "formula": data["formula"],
+        "replacement": data["replacement"],
+        "displayRoll": data["displayRoll"],
+        "results": data["results"],
+        "flags": {},
+        "sort": 0,
+        "ownership": {
+          "default": 0
+        }
+      }
+      print(newData)
       print(data['name'])
       with open(resultFolder + "/" + slugify(data['name']) + ".json", "w") as fw:
-        json.dump(data, fw, indent='\t')
+        json.dump(newData, fw, indent='\t')
 
