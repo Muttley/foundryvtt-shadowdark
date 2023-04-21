a50ead711f25c048f7627087d057aec402990731
Paul Maskelyne <mu77ley@gmail.com>
2023-04-21 13:05:39 +0100

    migrate Basic item spell scrolls to Scroll items

--- system/src/migrations/updates/_module.mjs
+++ system/src/migrations/updates/_module.mjs
@@ -2,3 +2,4 @@ export {default as Update_230325_1} from "./Update_230325_1.mjs";
 export {default as Update_230328_1} from "./Update_230328_1.mjs";
 export {default as Update_230329_1} from "./Update_230329_1.mjs";
 export {default as Update_230403_1} from "./Update_230403_1.mjs";
+export {default as Update_230417_2} from "./Update_230417_2.mjs";
