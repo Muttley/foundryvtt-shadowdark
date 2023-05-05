372f285c8a3592e730c13c252edb6270eb324809
Paul Maskelyne <mu77ley@gmail.com>
2023-05-05 15:57:23 +0100

    unpack leveldb

--- utils/lib/commands/pack.mjs
+++ utils/lib/commands/pack.mjs
@@ -1,11 +1,7 @@
-import {globSync} from "glob";
-import Datastore from "nedb-promises";
-import fs from "fs";
 import path from "path";
-import yaml from "js-yaml";
 import * as url from "url";
 
-import Logger from "../logger.mjs";
+import PackHandler from "../pack-handler.mjs";
 
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
@@ -33,88 +29,21 @@ export function getCommand() {
 
 			yargs.option("outputFormat", {
 				alias: "o",
-				default: "json",
-				describe: "Either 'yaml' or 'json'",
+				default: "nedb",
+				describe: "Either 'nedb' or 'leveldb'",
 				type: "string",
 			});
 
 			yargs.option("inputFormat", {
 				alias: "i",
-				default: "nedb",
-				describe: "Either 'nedb' or 'leveldb'",
+				default: "json",
+				describe: "Either 'yaml' or 'json'",
 				type: "string",
 			});
 		},
 		handler: async argv => {
-			if (!argv.source) {
-				Logger.error("You must specify a source");
-				return;
-			}
-
-			if (!argv.outputDir) {
-				Logger.error("You must specify an output directory");
-				return;
-			}
-
-			const isDataDirectory = argv.source.endsWith(".db")
-				&& fs.existsSync(argv.source)
-				&& fs.lstatSync(argv.source).isDirectory();
-
-			let dbDirectories = [argv.source];
-			if (!isDataDirectory) {
-				dbDirectories = globSync(`${argv.source}/*.db`);
-			}
-
-			for (const inputDbDirectory of dbDirectories) {
-				const packFileName = path.basename(inputDbDirectory);
-
-				if (!await fs.existsSync(argv.outputDir)) {
-					fs.mkdirSync(argv.outputDir, {recursive: true});
-				}
-
-				const outputDbFile = path.join(argv.outputDir, packFileName);
-
-				try {
-					if (argv.nedb || argv.all) {
-						await _packNeDb(inputDbDirectory, outputDbFile);
-					}
-					if (argv.leveldb || argv.all) {
-						await _pathClassicLevelDb(inputDbDirectory, outputDbFile);
-					}
-				}
-				catch(error) {
-					Logger.error(error);
-				}
-			}
+			const packer = new PackHandler(argv);
+			return await packer.pack();
 		},
 	};
-
-	async function _pathClassicLevelDb(inputDbDirectory, outputDbFile) {
-		Logger.log(`ClassicLevelDb::Pack ${inputDbDirectory} into ${outputDbFile}`);
-	}
-
-	async function _packNeDb(inputDbDirectory, outputDbFile) {
-		Logger.log(`NeDb::Pack ${inputDbDirectory} into ${outputDbFile}`);
-
-		const dbFileExists = await fs.existsSync(outputDbFile)
-			&& fs.lstatSync(outputDbFile).isFile();
-
-		if (dbFileExists) {
-			Logger.warn(`DB file ${outputDbFile} already exists and will be replaced`);
-			fs.unlinkSync(outputDbFile);
-		}
-
-		const db = Datastore.create(outputDbFile);
-
-		const files = fs.readdirSync(inputDbDirectory);
-		let inserted = 0;
-		for (const file of files) {
-			const rawData = fs.readFileSync(path.join(inputDbDirectory, file));
-			const dbEntry = file.endsWith(".yml") ? yaml.load(rawData) : JSON.parse(rawData);
-
-			await db.insert(dbEntry);
-			inserted++;
-		}
-		Logger.info(`Inserted ${inserted}/${files.length} records into ${outputDbFile}`);
-	}
 }
--- utils/lib/pack-handler.mjs
+++ utils/lib/pack-handler.mjs
@@ -1,6 +1,7 @@
-import Datastore from "nedb-promises";
+import {ClassicLevel} from "classic-level";
 import {globSync} from "glob";
-import fs from "fs";
+import Datastore from "nedb-promises";
+import fs, { readdirSync } from "fs";
 import path from "path";
 import yaml from "js-yaml";
 
@@ -16,50 +17,231 @@ export default class PackHandler {
 	}
 
 	async pack() {
+		if (!this._settingsOk("pack")) return;
+
 		switch (this.outputFormat) {
 			case "leveldb":
 				return await this._packLevelDb();
 			case "nedb":
 				return await this._packNeDb();
-			default:
-				Logger.error(`Unknown output format ${this.inputFormat}`);
 		}
 	}
 
 	async unpack() {
+		if (!this._settingsOk("unpack")) return;
+
 		switch (this.inputFormat) {
 			case "leveldb":
 				return await this._unpackLevelDb();
 			case "nedb":
 				return await this._unpackNeDb();
-			default:
-				Logger.error(`Unknown input format ${this.inputFormat}`);
 		}
+	}
+
+	_isFileLocked(filepath) {
+		try {
+			// Try to open the file for writing
+			const fd = fs.openSync(filepath, "w");
+
+			// If the file was successfully opened, it is not locked
+			fs.closeSync(fd);
+			return false;
+		}
+		catch(error) {
+			// If the file could not be opened, it is locked
+			if (error.code === "EBUSY") {
+				return true;
+			}
+			else {
+				throw error;
+			}
+		}
+	}
+
+	_settingsOk(task) {
+		let ok = true;
+
+		if (!this.source) {
+			Logger.error("You must specify a source");
+			ok = false;
+		}
+
+		if (!this.source) {
+			Logger.error("You must specify a source");
+			ok = false;
+		}
+
+		let inputFormats = [];
+		let outputFormats = [];
+
+		if (task === "pack") {
+			inputFormats = ["json", "yaml"];
+			outputFormats = ["leveldb", "nedb"];
+		}
+		else if (task === "unpack") {
+			inputFormats = ["leveldb", "nedb"];
+			outputFormats = ["json", "yaml"];
+		}
+
+		if (!outputFormats.includes(this.outputFormat)) {
+			Logger.error(`Unknown output format: ${this.outputFormat}`);
+			ok = false;
+		}
+
+		if (!inputFormats.includes(this.inputFormat)) {
+			Logger.error(`Unknown input format: ${this.inputFormat}`);
+			ok = false;
+		}
+
+		return ok;
 	}
 
 	async _packLevelDb() {
-		throw new Error("Implement Me!");
+		const isDataDirectory = this.source.endsWith(".db")
+			&& fs.existsSync(this.source)
+			&& fs.lstatSync(this.source).isDirectory();
+
+		let dbDirectories = [this.source];
+		if (!isDataDirectory) {
+			dbDirectories = globSync(`${this.source}/*.db`);
+		}
+
+		for (const inputDbDirectory of dbDirectories) {
+			const packFileName = path.basename(inputDbDirectory);
+
+			if (!await fs.existsSync(this.destination)) {
+				fs.mkdirSync(this.destination, {recursive: true});
+			}
+
+			const outputDbFile = path.join(this.destination, packFileName);
+
+			Logger.info(`LevelDb::Pack ${inputDbDirectory} into ${outputDbFile}`);
+
+			const dbFileExists = await fs.existsSync(outputDbFile)
+				&& fs.lstatSync(outputDbFile).isFile();
+
+			if (dbFileExists) {
+				Logger.warn(`DB file ${outputDbFile} already exists and will be replaced`);
+				fs.unlinkSync(outputDbFile);
+			}
+
+			const db = ClassicLevel(outputDbFile, {keyEncoding: "utf8", valueEncoding: "json"});
+
+			const files = fs.readdirSync(inputDbDirectory);
+			let inserted = 0;
+			for (const file of files) {
+				const rawData = fs.readFileSync(path.join(inputDbDirectory, file));
+				const dbEntry = file.endsWith(".yml") ? yaml.load(rawData) : JSON.parse(rawData);
+
+				await db.insert(dbEntry);
+				inserted++;
+			}
+			Logger.info(`Inserted ${inserted}/${files.length} records into ${outputDbFile}`);
+		}
 	}
 
 	async _packNeDb() {
-		throw new Error("Implement Me!");
+		const isDataDirectory = this.source.endsWith(".db")
+			&& fs.existsSync(this.source)
+			&& fs.lstatSync(this.source).isDirectory();
+
+		let dbDirectories = [this.source];
+		if (!isDataDirectory) {
+			dbDirectories = globSync(`${this.source}/*.db`);
+		}
+
+		for (const inputDbDirectory of dbDirectories) {
+			const packFileName = path.basename(inputDbDirectory);
+
+			if (!await fs.existsSync(this.destination)) {
+				fs.mkdirSync(this.destination, {recursive: true});
+			}
+
+			const outputDbFile = path.join(this.destination, packFileName);
+
+			Logger.info(`NeDb::Pack ${inputDbDirectory} into ${outputDbFile}`);
+
+			const dbFileExists = await fs.existsSync(outputDbFile)
+				&& fs.lstatSync(outputDbFile).isFile();
+
+			if (dbFileExists) {
+				Logger.warn(`DB file ${outputDbFile} already exists and will be replaced`);
+				fs.unlinkSync(outputDbFile);
+			}
+
+			const db = Datastore.create(outputDbFile);
+
+			const files = fs.readdirSync(inputDbDirectory);
+			let inserted = 0;
+			for (const file of files) {
+				const rawData = fs.readFileSync(path.join(inputDbDirectory, file));
+				const dbEntry = file.endsWith(".yml") ? yaml.load(rawData) : JSON.parse(rawData);
+
+				await db.insert(dbEntry);
+				inserted++;
+			}
+			Logger.info(`Inserted ${inserted}/${files.length} records into ${outputDbFile}`);
+		}
 	}
 
 	async _unpackLevelDb() {
-		throw new Error("Implement Me!");
+		const isDirectory = fs.existsSync(this.source)
+			&& fs.lstatSync(this.source).isDirectory();
+
+		const lockPath = path.join(this.source, "LOCK");
+
+		const isLevelDbDirectory = isDirectory && fs.existsSync(lockPath);
+
+		let compendiumDirs = [this.source];
+		if (!isLevelDbDirectory) {
+			compendiumDirs = readdirSync(this.source, {withFileTypes: true})
+				.filter(entry => entry.isDirectory())
+				.map(entry => path.join(this.source, entry.name));
+
+			compendiumDirs = compendiumDirs.filter(entry => {
+				return fs.existsSync(path.join(entry, "LOCK"));
+			});
+		}
+
+		for (const compendiumDir of compendiumDirs) {
+			const db = new ClassicLevel(
+				compendiumDir,
+				{
+					keyEncoding: "utf8",
+					valueEncoding: "json",
+				}
+			);
+			const basename = path.basename(compendiumDir);
+
+			const outputDir = path.join(this.destination, basename);
+
+			if (!fs.existsSync(outputDir)) {
+				fs.mkdirSync(outputDir, {recursive: true});
+			}
+
+			for await (const [key, value] of db.iterator()) {
+				const name = value.name
+					? `${value.name.replace(/[^a-z0-9]/gi, "_").toLowerCase()}`
+					: key;
+
+				value._key = key;
+
+				const basefileName = `${outputDir}/${name}`;
+				switch (this.outputFormat) {
+					case "yaml":
+						Logger.log(`Writing ${basefileName}.yml`);
+						fs.writeFileSync(`${basefileName}.yml`, yaml.dump(value));
+					case "json":
+						Logger.log(`Writing ${basefileName}.json`);
+						fs.writeFileSync(`${basefileName}.json`, JSON.stringify(value, null, 2));
+				}
+			}
+
+			await db.close();
+		}
 	}
 
 	async _unpackNeDb() {
-		if (!this.source) {
-			Logger.error("You must specify a source");
-			return;
-		}
-
-		if (!["json", "yaml"].includes(this.outputFormat)) {
-			Logger.error(`Unknown output format: ${this.outputFormat}`);
-			return;
-		}
-
 		const isDirectory = fs.existsSync(this.source)
 			&& fs.lstatSync(this.source).isDirectory();
 
@@ -86,16 +268,15 @@ export default class PackHandler {
 					? `${doc.name.replace(/[^a-z0-9]/gi, "_").toLowerCase()}`
 					: doc._id;
 
-				let fileName;
+				const basefileName = `${outputDir}/${name}`;
 				switch (this.outputFormat) {
 					case "yaml":
-						fileName = `${outputDir}/${name}.yml`;
-						fs.writeFileSync(fileName, yaml.dump(doc));
+						Logger.log(`Writing ${basefileName}.yml`);
+						fs.writeFileSync(`${basefileName}.yml`, yaml.dump(doc));
 					case "json":
-						fileName = `${outputDir}/${name}.json`;
-						fs.writeFileSync(fileName, JSON.stringify(doc, null, 2));
+						Logger.log(`Writing ${basefileName}.json`);
+						fs.writeFileSync(`${basefileName}.json`, JSON.stringify(doc, null, 2));
 				}
-				Logger.log(`${fileName} written`);
 			}
 		}
 	}
