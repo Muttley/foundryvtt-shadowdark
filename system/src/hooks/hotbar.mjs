fa165a4428a5fbaf734c814127aee9622983d8fb
Prototype <prototype444@hotmail.com>
2023-11-03 13:12:09 -0700

    Working for items, spells, ability, lightsources

--- system/src/hooks/hotbar.mjs
+++ system/src/hooks/hotbar.mjs
@@ -2,6 +2,7 @@ export const hotbarHooks = {
 	attach: () => {
 		Hooks.on("hotbarDrop", (bar, data, slot) => {
 			shadowdark.utils.createHotbarMacro(data, slot);
+			// Prevent other hooked events from running by returning false
 			return false;
 		  });
 	},
