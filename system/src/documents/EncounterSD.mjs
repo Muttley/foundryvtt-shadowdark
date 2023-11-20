1c0ad540bde6bdbf7027829a37299e03e066b71a
Chris Powell <cmpowell97@gmail.com>
2023-11-20 00:52:37 -0500

    fully working aside from translation

--- system/src/documents/EncounterSD.mjs
+++ system/src/documents/EncounterSD.mjs
@@ -1,54 +1,69 @@
 export default class EncounterSD extends Combat {
-	async rollInitiative(ids, options = {}) {
+	async rollInitiative(ids, {formula=null, updateTurn=true, messageOptions={}}={}) {
 		if (game.settings.get("shadowdark", "useClockwiseInitiative")) {
+			super.resetAll()
 			//Structure input data
 			ids = typeof  ids === 'string' ? [ids] : ids
+			const currentId = this.combatant?.id;
 			const chatRollMode = game.settings.get("core", "rollMode");
 
-			const combatants = ids.flatMap((id) => this.combatants.get(id) ?? []) // replace with this.combatants?
-			for (const combatant of combatants) {
-				super.setInitiative(combatant.id, null)
-			}
 			const rollers = []
 			const npcs = []
-			for (const combatant of combatants) {
+			for (const combatant of this.combatants) {
 				if (combatant.actor.type === 'Player') rollers.push(combatant)
 				else if (combatant.actor.type === 'NPC') npcs.push(combatant)
 			}
-
-
-			// npcs.sort((a, b) => b.actor.getRollData().initiativeBonus - a.actor.getRollData().initiativeBonus)
-
-			// const rollResults = await Promise.all(
-			// 	pcs.map(async (combatant) => {
-			// 		const pcActor = combatant.actor
-			// 		const rollData = combatant.actor.getRollData()
-			// 		const parts = [`${rollData.initiativeFormula}+${rollData.initiativeBonus}`]
-			// 		const result = (await CONFIG.DiceSD._roll(parts, pcActor)).roll.total
-			// 		return {id: combatant.id, prelimInitiative: result}
-			// 	})
-			// )
-
-
 			if (npcs.length > 0) {
-				const npcRoller = npcs.reduce(function(prev, current) {
-					return (prev && prev.getRollData().initiativeBonusy > current.getRollData().initiativeBonus)
-					? prev : current
-				})
-				rollers.push(npcRoller)
-				// const npcRollData = npcRoller.getRollData()
-				// const npcParts = [`${npcRollData.initiativeFormula}+${npcRollData.initiativeBonus}`]
-				// const npcRollResult = (await CONFIG.DiceSD._roll(npcParts, npcLeader)).roll.total
-				// rollResults.push({id: npcs[0].id, prelimInitiative: npcRollResult})
+				npcs.sort((a, b) => b.actor.getRollData().initiativeBonus - a.actor.getRollData().initiativeBonus)
+				rollers.push(npcs[0])
 			}
 
+			const messages = [];
+			const rollResults = await Promise.all(
+				rollers.map(async (combatant, index) => {
+					const isNPC = combatant.actor.type === 'NPC'
+					const roll = combatant.getInitiativeRoll(formula)
+					await roll.evaluate({async: true})
 
+					// Construct chat message data
+					let messageData = foundry.utils.mergeObject({
+					speaker: ((isNPC) ?
+					null :
+					ChatMessage.getSpeaker({
+						actor: combatant.actor,
+						token: combatant.token,
+						alias: combatant.name
+					})),
+					flavor: game.i18n.format("COMBAT.RollsInitiative", {name: ((isNPC) ? 'The Gamemaster' : combatant.name)}),
+					flags: {"core.initiativeRoll": true}
+					}, messageOptions);
+					const rollChatData = await roll.toMessage(messageData, {create: false});
 
-			rollResults.sort((a,b) => b.prelimInitiative - a.prelimInitiative)
+					// If the combatant is hidden, use a private roll unless an alternative rollMode was explicitly requested
+					rollChatData.rollMode = "rollMode" in messageOptions ? messageOptions.rollMode
+					: (combatant.hidden ? CONST.DICE_ROLL_MODES.PRIVATE : chatRollMode );
 
-			const firstCombatant = combatants.get(rollResults[0].id)
-			const firstInitiative = rollResults[0].prelimInitiative
-			const activeUsers = [game.users.activeGM, ...(game.users.players.filter((p) => p.active))]
+					// Play 1 sound for the whole rolled set
+					if ( index > 0 ) rollChatData.sound = null;
+					messages.push(rollChatData);
+
+					return {combatant: combatant, prelimInitiative: roll.total}
+				})
+			)
+
+			console.log(rollResults)
+
+			const firstCombatant = rollResults.reduce(function(prev, current) {
+				return (prev && prev.prelimInitiative > current.prelimInitiative)
+				? prev : current
+			}).combatant
+////////////
+
+/////////////
+			const activeUsers = [
+				game.users.activeGM,
+				...(game.users.players.filter((p) => this.combatants.find((c) => c.actor === p.character)))
+			]
 
 			const firstIndex = (firstCombatant.actor.type === 'NPC') ?
 				activeUsers.findIndex((p) => p === game.users.activeGM) :
@@ -56,21 +71,44 @@ export default class EncounterSD extends Combat {
 			for (let i = 0; i < firstIndex; i++) {
 				activeUsers.push(activeUsers.shift())
 			}
+			console.log(firstCombatant)
+			console.log(activeUsers)
 
 			const initiativeOrder = []
 
 			for (const user of activeUsers) {
 				if (user === game.users.activeGM) initiativeOrder.push(...npcs)
 				else {
-					const c = combatants.get(user.character?.id)
+					const c = this.combatants.find((c) => c.actor === user.character)
 					if (c) initiativeOrder.push(c)
 				}
 			}
+			console.log(initiativeOrder)
+
 
 			for (let i = 0; i < initiativeOrder.length; i++) {
-				super.setInitiative(initiativeOrder[i].id, initiativeOrder.size - i)
+				super.setInitiative(initiativeOrder[i].id, initiativeOrder.length - i)
 			}
+
+			// Ensure the turn order remains with the same combatant
+			if ( updateTurn && currentId ) {
+				await this.update({turn: this.turns.findIndex(t => t.id === currentId)});
+			}
+
+			// Create multiple chat messages
+			await ChatMessage.implementation.create(messages);
+						// Construct chat message data
+						let messageData = foundry.utils.mergeObject({
+							// speaker: (ChatMessage.getSpeaker({
+							// 	actor: combatant.actor,
+							// 	token: combatant.token,
+							// 	alias: combatant.name
+							// })),
+							flavor: `Since <strong>${(firstCombatant.actor.type === 'Player' ? firstCombatant.name : 'the Gamemaster')}</strong> rolled highest, they will go first.`,
+							}, messageOptions);
+			await ChatMessage.implementation.create(messageData);
+			return this;
 		}
-		else super.rollInitiative(ids, options)
+		else super.rollInitiative(ids, {formula, updateTurn, messageOptions})
 	}
 }
