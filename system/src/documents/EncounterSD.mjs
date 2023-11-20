6c0726f3d8893b584d3575bd6e1ef8b1433909d6
Chris Powell <cmpowell97@gmail.com>
2023-11-20 01:41:27 -0500

    implement i18n and comment code

--- system/src/documents/EncounterSD.mjs
+++ system/src/documents/EncounterSD.mjs
@@ -1,12 +1,15 @@
 export default class EncounterSD extends Combat {
 	async rollInitiative(ids, {formula=null, updateTurn=true, messageOptions={}}={}) {
+		// Roll clockwise initiative (rulebook page 83) if that setting is selected
 		if (game.settings.get("shadowdark", "useClockwiseInitiative")) {
+			// Clear existing initiatives
 			super.resetAll()
-			//Structure input data
-			ids = typeof  ids === 'string' ? [ids] : ids
+
+			// Structure input data
 			const currentId = this.combatant?.id;
 			const chatRollMode = game.settings.get("core", "rollMode");
 
+			// Initiative is rolled by all PCs and only the NPC with the highest bonus
 			const rollers = []
 			const npcs = []
 			for (const combatant of this.combatants) {
@@ -18,23 +21,23 @@ export default class EncounterSD extends Combat {
 				rollers.push(npcs[0])
 			}
 
+			// Get roll results for all combatants who get to roll initiative
 			const messages = [];
 			const rollResults = await Promise.all(
 				rollers.map(async (combatant, index) => {
-					const isNPC = combatant.actor.type === 'NPC'
 					const roll = combatant.getInitiativeRoll(formula)
 					await roll.evaluate({async: true})
 
 					// Construct chat message data
 					let messageData = foundry.utils.mergeObject({
-					speaker: ((isNPC) ?
+					speaker: ((combatant.actor.type === 'NPC') ?
 					null :
 					ChatMessage.getSpeaker({
 						actor: combatant.actor,
 						token: combatant.token,
 						alias: combatant.name
 					})),
-					flavor: game.i18n.format("COMBAT.RollsInitiative", {name: ((isNPC) ? 'The Gamemaster' : combatant.name)}),
+					flavor: game.i18n.format("COMBAT.RollsInitiative", {name: ((combatant.actor.type === 'NPC') ? `${game.i18n.localize("SHADOWDARK.dialog.gm")}` : combatant.name)}),
 					flags: {"core.initiativeRoll": true}
 					}, messageOptions);
 					const rollChatData = await roll.toMessage(messageData, {create: false});
@@ -51,41 +54,38 @@ export default class EncounterSD extends Combat {
 				})
 			)
 
-			console.log(rollResults)
-
+			// Identify the combatant with the highest initiative
 			const firstCombatant = rollResults.reduce(function(prev, current) {
 				return (prev && prev.prelimInitiative > current.prelimInitiative)
 				? prev : current
 			}).combatant
-////////////
 
-/////////////
+			// Only consider users who have a character fighting in the current combat
 			const activeUsers = [
 				game.users.activeGM,
 				...(game.users.players.filter((p) => this.combatants.find((c) => c.actor === p.character)))
 			]
 
+			// Rearrange user list so that the first combatant's owner goes first, and the rest follow in a predetermined order
 			const firstIndex = (firstCombatant.actor.type === 'NPC') ?
 				activeUsers.findIndex((p) => p === game.users.activeGM) :
 				activeUsers.findIndex((p) => p?.character === firstCombatant.actor)
 			for (let i = 0; i < firstIndex; i++) {
 				activeUsers.push(activeUsers.shift())
 			}
-			console.log(firstCombatant)
-			console.log(activeUsers)
 
+			// Sort combatants according to their owner's position in the clockwise initiative order.
 			const initiativeOrder = []
-
 			for (const user of activeUsers) {
+				// All NPCs go on the GM's turn in order of descending initiative bonus
 				if (user === game.users.activeGM) initiativeOrder.push(...npcs)
 				else {
 					const c = this.combatants.find((c) => c.actor === user.character)
 					if (c) initiativeOrder.push(c)
 				}
 			}
-			console.log(initiativeOrder)
 
-
+			// Set all combatants' initiatives in descending order
 			for (let i = 0; i < initiativeOrder.length; i++) {
 				super.setInitiative(initiativeOrder[i].id, initiativeOrder.length - i)
 			}
@@ -95,20 +95,22 @@ export default class EncounterSD extends Combat {
 				await this.update({turn: this.turns.findIndex(t => t.id === currentId)});
 			}
 
-			// Create multiple chat messages
-			await ChatMessage.implementation.create(messages);
-						// Construct chat message data
-						let messageData = foundry.utils.mergeObject({
-							// speaker: (ChatMessage.getSpeaker({
-							// 	actor: combatant.actor,
-							// 	token: combatant.token,
-							// 	alias: combatant.name
-							// })),
-							flavor: `Since <strong>${(firstCombatant.actor.type === 'Player' ? firstCombatant.name : 'the Gamemaster')}</strong> rolled highest, they will go first.`,
-							}, messageOptions);
+			// Post initiative rolls to chat
+			await ChatMessage.implementation.create(messages)
+
+			// Post a reminder to the chat that clockwise initiative is active
+			let messageData = foundry.utils.mergeObject({
+				flavor: game.i18n.format("SHADOWDARK.chat.clockwise_initiative", {
+					name: ((firstCombatant.actor.type === 'NPC') ?
+					`${game.i18n.localize("USER.RoleGamemaster")}` :
+					firstCombatant.name)
+				})
+			}, messageOptions);
 			await ChatMessage.implementation.create(messageData);
 			return this;
 		}
+
+		// If circular initiative is not selected, use the default Foundry method
 		else super.rollInitiative(ids, {formula, updateTurn, messageOptions})
 	}
 }
