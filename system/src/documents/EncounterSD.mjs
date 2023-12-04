468ce96044ebf72d9aede526b2789485b83e1d75
Chris Powell <cmpowell97@gmail.com>
2023-12-03 20:05:50 -0500

    Revert "change clockwise initiative to alphabetical order"
    This reverts commit 424f528beed5ba9016fda193947c8242c1c968c1.

--- system/src/documents/EncounterSD.mjs
+++ system/src/documents/EncounterSD.mjs
@@ -1,121 +1,117 @@
 export default class EncounterSD extends Combat {
-	async rollInitiative(ids, { formula = null, updateTurn = true, messageOptions = {} } = {}) {
+	async rollInitiative(ids, {formula=null, updateTurn=true, messageOptions={}}={}) {
 		// Roll clockwise initiative (rulebook page 83) if that setting is selected
 		if (game.settings.get("shadowdark", "useClockwiseInitiative")) {
 			// Clear existing initiatives
-			super.resetAll();
+			super.resetAll()
 
 			// Structure input data
 			const currentId = this.combatant?.id;
 			const chatRollMode = game.settings.get("core", "rollMode");
 
 			// Initiative is rolled by all PCs and only the NPC with the highest bonus
-			const rollers = [];
-			const npcs = [];
-			const others = [];
+			const rollers = []
+			const npcs = []
 			for (const combatant of this.combatants) {
-				if (combatant.actor?.type === "Player") rollers.push(combatant);
-				else if (combatant.actor?.type === "NPC") npcs.push(combatant);
-				else others.push(combatant); // Group lights and combatants who lack actors
+				if (combatant.actor.type === "Player") rollers.push(combatant)
+				else if (combatant.actor.type === "NPC") npcs.push(combatant)
 			}
-			// Sort PC and "other" combatants by alphabetical order
-			rollers.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
-			others.sort((a, b) => (a?.name.toLowerCase() || "").localeCompare((b?.name.toLowerCase() || "")));
 			if (npcs.length > 0) {
 				npcs.sort((a, b) =>
-					b.actor.getRollData().initiativeBonus - a.actor.getRollData().initiativeBonus);
-				rollers.push(npcs[0]);
+					b.actor.getRollData().initiativeBonus - a.actor.getRollData().initiativeBonus)
+				rollers.push(npcs[0])
 			}
 
 			// Get roll results for all combatants who get to roll initiative
 			const messages = [];
 			const rollResults = await Promise.all(
 				rollers.map(async (combatant, index) => {
-					if (!combatant.actor) {
-						return { combatant: combatant, prelimInitiative: 0 };
-					}
-					const roll = combatant.getInitiativeRoll(formula);
-					await roll.evaluate({ async: true });
+					const roll = combatant.getInitiativeRoll(formula)
+					await roll.evaluate({async: true})
 
 					// Construct chat message data
 					let messageData = foundry.utils.mergeObject({
-						speaker: ((combatant.actor.type === "NPC")
-							? null
-							: ChatMessage.getSpeaker({
+						speaker: ((combatant.actor.type === "NPC") ?
+							null :
+							ChatMessage.getSpeaker({
 								actor: combatant.actor,
 								token: combatant.token,
-								alias: combatant.name,
+								alias: combatant.name
 							})),
-						flavor: game.i18n.format("COMBAT.RollsInitiative", { name: ((combatant.actor.type === "NPC") ? `${game.i18n.localize("SHADOWDARK.dialog.gm")}` : combatant.name) }),
-						flags: { "core.initiativeRoll": true },
+						flavor: game.i18n.format("COMBAT.RollsInitiative", {name: ((combatant.actor.type === "NPC") ? `${game.i18n.localize("SHADOWDARK.dialog.gm")}` : combatant.name)}),
+						flags: {"core.initiativeRoll": true}
 					}, messageOptions);
-					const rollChatData = await roll.toMessage(messageData, { create: false });
+					const rollChatData = await roll.toMessage(messageData, {create: false});
 
-					/* If the combatant is hidden, use a private roll
-					unless an alternative rollMode was requested */
+					// If the combatant is hidden, use a private roll unless an alternative rollMode was explicitly requested
 					rollChatData.rollMode = "rollMode" in messageOptions ? messageOptions.rollMode
-						: (combatant.hidden ? CONST.DICE_ROLL_MODES.PRIVATE : chatRollMode);
+						: (combatant.hidden ? CONST.DICE_ROLL_MODES.PRIVATE : chatRollMode );
 
 					// Play 1 sound for the whole rolled set
-					if (index > 0) rollChatData.sound = null;
+					if ( index > 0 ) rollChatData.sound = null;
 					messages.push(rollChatData);
 
-					return { combatant: combatant, prelimInitiative: roll.total };
+					return {combatant: combatant, prelimInitiative: roll.total}
 				})
-			);
+			)
 
 			// Identify the combatant with the highest initiative
 			const firstCombatant = rollResults.reduce(function(prev, current) {
 				return (prev && prev.prelimInitiative > current.prelimInitiative)
-					? prev : current;
-			}).combatant;
-			const firstIndex = (firstCombatant.actor.type === "NPC")
-				? rollers.findIndex(c => c?.actor.type === "NPC")
-				: rollers.findIndex(c => c?.actor === firstCombatant.actor);
+					? prev : current
+			}).combatant
+
+			// Only consider users who have a character fighting in the current combat
+			const activeUsers = [
+				game.users.activeGM,
+				...(game.users.players.filter((p) => this.combatants.find((c) => c.actor === p.character)))
+			]
+
+			// Rearrange user list so that the first combatant's owner goes first, and the rest follow in a predetermined order
+			const firstIndex = (firstCombatant.actor.type === "NPC") ?
+				activeUsers.findIndex((p) => p === game.users.activeGM) :
+				activeUsers.findIndex((p) => p?.character === firstCombatant.actor)
 			for (let i = 0; i < firstIndex; i++) {
-				rollers.push(rollers.shift());
+				activeUsers.push(activeUsers.shift())
 			}
 
-			// Create the final initiative order as an array
-			const initiativeOrder = [];
-			for (const roller of rollers) {
-				/* All NPCs, lights, and combatants without actors go on the GM's turn
-				in order of descending initiative bonus */
-				if (roller?.actor.type !== "Player") {
-					initiativeOrder.push(...npcs);
-					initiativeOrder.push(...others);
-				}
+			// Sort combatants according to their owner's position in the clockwise initiative order.
+			const initiativeOrder = []
+			for (const user of activeUsers) {
+				// All NPCs go on the GM's turn in order of descending initiative bonus
+				if (user === game.users.activeGM) initiativeOrder.push(...npcs)
 				else {
-					initiativeOrder.push(roller);
+					const c = this.combatants.find((c) => c.actor === user.character)
+					if (c) initiativeOrder.push(c)
 				}
 			}
 
 			// Set all combatants' initiatives in descending order
 			for (let i = 0; i < initiativeOrder.length; i++) {
-				super.setInitiative(initiativeOrder[i].id, initiativeOrder.length - i);
+				super.setInitiative(initiativeOrder[i].id, initiativeOrder.length - i)
 			}
 
 			// Ensure the turn order remains with the same combatant
-			if (updateTurn && currentId) {
-				await this.update({ turn: this.turns.findIndex(t => t.id === currentId) });
+			if ( updateTurn && currentId ) {
+				await this.update({turn: this.turns.findIndex(t => t.id === currentId)});
 			}
 
 			// Post initiative rolls to chat
-			await ChatMessage.implementation.create(messages);
+			await ChatMessage.implementation.create(messages)
 
 			// Post a reminder to the chat that clockwise initiative is active
 			let messageData = foundry.utils.mergeObject({
 				flavor: game.i18n.format("SHADOWDARK.chat.clockwise_initiative", {
-					name: ((firstCombatant.actor.type === "NPC")
-						? `${game.i18n.localize("USER.RoleGamemaster")}`
-						: firstCombatant.name),
-				}),
+					name: ((firstCombatant.actor.type === "NPC") ?
+						`${game.i18n.localize("USER.RoleGamemaster")}` :
+						firstCombatant.name)
+				})
 			}, messageOptions);
 			await ChatMessage.implementation.create(messageData);
 			return this;
 		}
 
-		// If clockwise initiative is not selected, use the default Foundry method instead
-		else super.rollInitiative(ids, { formula, updateTurn, messageOptions });
+		// If circular initiative is not selected, use the default Foundry method
+		else super.rollInitiative(ids, {formula, updateTurn, messageOptions})
 	}
 }
