cdfd6c6668d205166449170bf20214ceaa28118b
Paul Maskelyne <mu77ley@gmail.com>
2023-12-16 23:32:54 +0000

    unneeded now compendiums migrated

--- system/src/migrations/updates/Update_231216_1.mjs
+++ system/src/migrations/updates/Update_231216_1.mjs
@@ -8,7 +8,6 @@ export default class Update_231216_1 extends UpdateBaseSD {
 
 		const updateData = {
 			"system.source.-=page": null,
-			"system.source.title": "quickstart",
 		};
 
 		return updateData;
