9116594fbf231972e730e46497979fb2bdb625fd
Paul Maskelyne <mu77ley@gmail.com>
2023-12-20 22:07:11 +0000

    closes #650

--- system/src/hooks/hotbar.mjs
+++ system/src/hooks/hotbar.mjs
@@ -1,10 +1,11 @@
 export const hotbarHooks = {
 	attach: () => {
 		Hooks.on("hotbarDrop", (bar, data, slot) => {
-			if (data.type === "Item") {
-				shadowdark.utils.createHotbarMacro(data, slot);
-				return false;
-			}
+			if (data.type !== "Item") return true;
+			if (data.type === "Macro" || data.type === "RollTable") return true;
+
+			shadowdark.utils.createHotbarMacro(data, slot);
+			return false;
 		});
 	},
 };
