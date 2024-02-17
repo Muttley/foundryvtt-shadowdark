36abc5429844e979a5f68e5b500c618ed91ab815
Chris Powell <cmpowell97@gmail.com>
2024-02-17 00:28:08 -0500

    Adds the basics of an item importer that can interpret item descriptions copied from a PDF. Handles weapons and armor. Lacks instructions on the UI.

--- system/src/apps/_module.mjs
+++ system/src/apps/_module.mjs
@@ -1,6 +1,7 @@
 // Property selector applications
 export {default as EffectPanelSD} from "./EffectPanelSD.mjs";
 export {default as GemBagSD} from "./GemBagSD.mjs";
+export {default as ItemImporterSD} from "./ItemImporterSD.mjs";
 export {default as LightSourceTrackerSD} from "./LightSourceTrackerSD.mjs";
 export {default as MonsterImporterSD} from "./MonsterImporterSD.mjs";
 export {default as NpcAttackRangesSD} from "./NpcAttackRangesSD.mjs";
