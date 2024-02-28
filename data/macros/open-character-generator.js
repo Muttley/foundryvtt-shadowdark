2c9f43acbe06554e2fc852fe69fb9e5e02fafbfe
Prototype <prototype444@hotmail.com>
2024-02-28 00:11:31 -0800

    UI changes

--- data/macros/open-character-generator.js
+++
@@ -1,6 +0,0 @@
-/**
- ****************************************************************
- * This macro opens a preview version of the character generator
- ***************************************************************/
-
-new shadowdark.apps.CharacterGeneratorSD().render(true);
