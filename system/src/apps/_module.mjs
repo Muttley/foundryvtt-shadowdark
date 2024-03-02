de2bbee3e272d2aa3a4425dd34663037713fbf60
Paul Maskelyne <mu77ley@gmail.com>
2024-03-02 11:34:42 +0000

    sorting

--- system/src/apps/_module.mjs
+++ system/src/apps/_module.mjs
@@ -1,4 +1,5 @@
 // Property selector applications
+export {default as CharacterGeneratorSD} from "./CharacterGeneratorSD.mjs";
 export {default as EffectPanelSD} from "./EffectPanelSD.mjs";
 export {default as GemBagSD} from "./GemBagSD.mjs";
 export {default as ItemImporterSD} from "./ItemImporterSD.mjs";
@@ -6,5 +7,4 @@ export {default as LightSourceTrackerSD} from "./LightSourceTrackerSD.mjs";
 export {default as MonsterImporterSD} from "./MonsterImporterSD.mjs";
 export {default as NpcAttackRangesSD} from "./NpcAttackRangesSD.mjs";
 export {default as ShadowdarklingImporterSD} from "./ShadowdarklingImporterSD.mjs";
-export {default as CharacterGeneratorSD} from "./CharacterGeneratorSD.mjs";
 export {default as SpellImporterSD} from "./SpellImporterSD.mjs";
