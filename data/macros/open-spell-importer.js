9fa7ab935beb7ace5d829120f292b6e4cc67a674
Chris Powell <cmpowell97@gmail.com>
2024-03-01 21:14:53 -0500

    Update open-spell-importer.js
    Signed-off-by: Chris Powell <cmpowell97@gmail.com>

--- data/macros/open-spell-importer.js
+++ data/macros/open-spell-importer.js
@@ -1,6 +1,6 @@
 /**
  ****************************************************************
- * This macro can be used to open the monster importer
+ * This macro can be used to open the spell importer
  ***************************************************************/
 
 new shadowdark.apps.SpellImporterSD().render(true);
