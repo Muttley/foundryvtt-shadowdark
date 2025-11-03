dbe988e1f77aa1efd4fa3cad9f0e6cb71a773033
Sam Balana <sbalana@google.com>
2025-11-03 03:55:47 -0800

    Destructure fields

--- system/src/models/VehicleSD.mjs
+++ system/src/models/VehicleSD.mjs
@@ -1,33 +1,37 @@
 import { ActorBaseSD } from "./SDBase.mjs";
 
-const fields = foundry.data.fields;
+const { ArrayField, DocumentUUIDField, NumberField, SchemaField } =
+	foundry.data.fields;
 
 export default class VehicleSD extends ActorBaseSD {
 	static defineSchema() {
-
 		const schema = {
-			attributes: new fields.SchemaField({
-				hp: new fields.SchemaField({
-					value: new fields.NumberField({ integer: true, initial: 0, min: 0}),
-					max: new fields.NumberField({ integer: true, initial: 0, min: 0}),
+			attributes: new SchemaField({
+				hp: new SchemaField({
+					value: new NumberField({ integer: true, initial: 0, min: 0 }),
+					max: new NumberField({ integer: true, initial: 0, min: 0 }),
 				}),
-				ac: new fields.SchemaField({
-					value: new fields.NumberField({integer: true, initial: 10, min: 0}),
+				ac: new SchemaField({
+					value: new NumberField({ integer: true, initial: 10, min: 0 }),
 				}),
-				speed: new fields.NumberField({ integer: true, initial: 2, min: 0}),
-				slotsPerHp: new fields.NumberField({ integer: true, initial: 10, min: 0}),
-				slotsPerPassenger: new fields.NumberField({ integer: true, initial: 10, min: 0}),
+				speed: new NumberField({ integer: true, initial: 2, min: 0 }),
+				slotsPerHp: new NumberField({ integer: true, initial: 10, min: 0 }),
+				slotsPerPassenger: new NumberField({
+					integer: true,
+					initial: 10,
+					min: 0,
+				}),
 			}),
-			coins: new fields.SchemaField({
-				gp: new fields.NumberField({ integer: true, initial: 0, min: 0}),
-				sp: new fields.NumberField({ integer: true, initial: 0, min: 0}),
-				cp: new fields.NumberField({ integer: true, initial: 0, min: 0}),
+			coins: new SchemaField({
+				gp: new NumberField({ integer: true, initial: 0, min: 0 }),
+				sp: new NumberField({ integer: true, initial: 0, min: 0 }),
+				cp: new NumberField({ integer: true, initial: 0, min: 0 }),
 			}),
-			slots: new fields.NumberField({ integer: true, initial: 0, min: 0}),
-			passengers: new fields.NumberField({ integer: true, initial: 0, min: 0}),
-			properties: new fields.ArrayField(new fields.DocumentUUIDField()),
+			slots: new NumberField({ integer: true, initial: 0, min: 0 }),
+			passengers: new NumberField({ integer: true, initial: 0, min: 0 }),
+			properties: new ArrayField(new DocumentUUIDField()),
 		};
 
-		return Object.assign(super.defineSchema(), schema);
+		return Object.assign(ActorBaseSD.defineSchema(), schema);
 	}
 }
