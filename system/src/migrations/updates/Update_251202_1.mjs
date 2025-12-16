8499eab129b95207ed337a502980bfb42333ce9c
Britton Johnson <brittonlj@gmail.com>
2025-12-15 19:14:31 -0600

    Added more migrations

--- system/src/migrations/updates/Update_251202_1.mjs
+++ system/src/migrations/updates/Update_251202_1.mjs
@@ -14,25 +14,55 @@ export default class Update_251202_1 extends UpdateBaseSD {
 						// check initiative
 						if (change.value === "initiative") {
 							change.key = "system.roll.initiative.advantage";
-							change.mode = 2;
+							change.mode = CONST.ACTIVE_EFFECT_MODES.ADD;
 							change.value = 1;
 							continue;
 						}
 
 					case "system.bonuses.acBonus":
 						change.key = "system.attributes.ac.value";
-						change.mode = 2;
+						change.mode = CONST.ACTIVE_EFFECT_MODES.ADD;
 						continue;
 
 					case "system.bonuses.acBonusFromAttribute":
 						change.key = "system.attributes.ac.value";
-						change.mode = 2;
+						change.mode = CONST.ACTIVE_EFFECT_MODES.ADD;
 						change.value = `@attributes.${change.value}.mod`;
 						continue;
 
 					case "system.bonuses.gearSlots":
 						change.key = "system.slots";
-						change.mode = 2;
+						change.mode = CONST.ACTIVE_EFFECT_MODES.ADD;
+						continue;
+
+					case "system.bonuses.meleeAttackBonus":
+						change.key = "system.roll.melee.bonus.this";
+						change.mode = CONST.ACTIVE_EFFECT_MODES.ADD;
+						continue;
+
+					case "system.bonuses.meleeDamageBonus":
+						change.key = "system.roll.melee.damage.this";
+						change.mode = CONST.ACTIVE_EFFECT_MODES.ADD;
+						continue;
+
+					case "system.bonuses.rangedAttackBonus":
+						change.key = "system.roll.ranged.bonus.this";
+						change.mode = CONST.ACTIVE_EFFECT_MODES.ADD;
+						continue;
+
+					case "system.bonuses.rangedDamageBonus":
+						change.key = "system.roll.ranged.damage.this";
+						change.mode = CONST.ACTIVE_EFFECT_MODES.ADD;
+						continue;
+
+					case "system.bonuses.spellcastingCheckBonus":
+						change.key = "system.roll.spell.advantage.all";
+						change.mode = CONST.ACTIVE_EFFECT_MODES.ADD;
+						continue;
+
+					case "system.bonuses.unarmoredAcBonus":
+						change.key = "system.attributes.ac.unarmored";
+						change.mode = CONST.ACTIVE_EFFECT_MODES.ADD;
 						continue;
 
 				}
